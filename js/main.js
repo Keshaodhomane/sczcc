ed6ad9c3413f4ab994709bfd7bd11eb1e4692e54
Keshao Dhomane <keshaodhomane@gmail.com>
2018-07-17 20:33:07 +0530

    main js upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-$('[data-slick]').slick({
-    fade: true,
-    autoplay:true,
-    speed: 300,
-    nextArrow: '<span class="rightarrow"> <i class="fa fa-arrow-right "></i> </span>',
-    prevArrow: '<span class="nextarrow"> <i class="fa fa-arrow-left "></i> </span>'
-  });
 
 
 
@@ -34,7 +27,7 @@ $('[data-slick]').slick({
 
 
 $('.govt-slider').slick({
-    slidesToShow: 1,
+    slidesToShow: 5,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
@@ -170,5 +163,51 @@ myAudio.onpause = function() {
 $("#mus").click(function(){
 
     $(this).toggleClass("music-icon-off");
+
+});
+
+
+
+
+// Slider Options
+$(".Modern-Slider").slick({
+    autoplay: true,
+    autoplaySpeed: 4000,
+    speed: 800,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    pauseOnHover: false,
+    dots: true,
+    pauseOnDotsHover: true,
+    cssEase: 'linear',
+    fade: true,
+    draggable: false,
+    prevArrow: '<button class="PrevArrow"> <span class="Thumbnail"></span></button>',
+    nextArrow: '<button class="NextArrow"> <span class="Thumbnail"></span></button>',
+    // Custom Dots With Thumbnails Tool Tip
+    customPaging: function (slider, i) {
+        var thumbnail = $(slider.$slides[i]).data('thumbnail');
+        return '<a href="javascript:void(0)"><img src="' + thumbnail + '"></a>';
+    },
+});
+
+// Arrows Thumbnails Trick
+var PrevThumbinalFirst = $(".Modern-Slider .item:last-child").children(".img-fill").children("img").attr("src");
+$(".Modern-Slider .PrevArrow .Thumbnail").css({
+    "background-image": "url(" + PrevThumbinalFirst + ")"
+})
+var NextThumbinalFirst = $(".Modern-Slider .item:first-child").next().children(".img-fill").children("img").attr("src");
+$(".Modern-Slider .NextArrow .Thumbnail").css({
+    "background-image": "url(" + NextThumbinalFirst + ")"
+})
+$(".Modern-Slider").on('afterChange', function (event, slick, currentSlide) {
+    var PrevThumbinal = $(".Modern-Slider .item.slick-active").prev(".item").children(".img-fill").children("img").attr("src");
+    $(".Modern-Slider .PrevArrow .Thumbnail").css({
+        "background-image": "url(" + PrevThumbinal + ")"
+    })
+    var NextThumbinal = $(".Modern-Slider .item.slick-active").next(".item").children(".img-fill").children("img").attr("src");
+    $(".Modern-Slider .NextArrow .Thumbnail").css({
+        "background-image": "url(" + NextThumbinal + ")"
+    })
 
 });
